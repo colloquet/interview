a6f98f0c4c5149c5368c18acb982a916beaff37c
Colloque Tsui <colloque.tsui@mioying.com>
2020-07-20 18:49:32 +0800

    update

--- calculator.js
+++ calculator.js
@@ -1,1 +1,20 @@
 const str = '34-24134-341324+324123';
+
+function calculate(input) {
+  const array = str.split(/(-|\+)/g);
+  
+  while (array.length > 1) {
+    let result = 0;
+    let left = array.shift();
+    let operator = array.shift();
+    let right = array.shift();
+  
+    result = Number(left) + (right * (operator === '-' ? -1 : 1));
+    array.unshift(result);
+  }
+
+  return array[0];
+}
+
+console.log(calculate(str));
+console.log(eval(str));
