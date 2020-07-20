812125101f21c336d3ee651a2af080deecd7410b
Colloque Tsui <colloquet@gmail.com>
2020-07-20 12:39:17 +0800

    update

--- curry.js
+++ curry.js
@@ -1,23 +1,52 @@
-function sum(a, b, c, d, e) {
-  return a + b + c + d + e;
-}
+// function sum(a, b, c, d, e) {
+//   return a + b + c + d + e;
+// }
 
-function curry(fn) {
-  return function curried(...args) {
-    if (args.length >= fn.length) {
-      return fn.apply(this, args);
-    } else {
-      return function (...args2) {
-        return curried.apply(this, [...args, ...args2]);
-      };
+// function curry(fn) {
+//   return function curried(...args) {
+//     if (args.length >= fn.length) {
+//       return fn.apply(this, args);
+//     } else {
+//       return function (...args2) {
+//         return curried.apply(this, [...args, ...args2]);
+//       };
+//     }
+//   };
+// }
+
+// function sum1(...args) {
+//   return args.reduce((total, item) => total + item, 0);
+// }
+
+// function currying(fn, ...args) {
+//   if (args.length >= fn.length) {
+//     return fn(...args);
+//   }
+
+//   return function (...args2) {
+//     return currying(fn, ...args, ...args2);
+//   };
+// }
+
+// const curriedSum = curry(sum);
+
+// console.log(curriedSum(1, 2, 3, 4, 5));
+// console.log(curriedSum(1)(2)(3)(4)(5));
+// console.log(curriedSum(1, 2)(3)(4)(5));
+// console.log(curriedSum(1, 2, 3)(4)(5));
+// console.log(curriedSum(1, 2, 3)(4, 5));
+
+// console.log(curriedSum1(1)(2));
+
+function sum(...args) {
+  return function(...args1) {
+    if (!args1.length) {
+      return args.reduce((total, item) => total + item, 0);
     }
-  };
+    return sum(...args, ...args1);
+  }
 }
 
-const curriedSum = curry(sum);
-
-console.log(curriedSum(1, 2, 3, 4, 5));
-console.log(curriedSum(1)(2)(3)(4)(5));
-console.log(curriedSum(1, 2)(3)(4)(5));
-console.log(curriedSum(1, 2, 3)(4)(5));
-console.log(curriedSum(1, 2, 3)(4, 5));
+console.log(sum(1, 2, 3)());
+console.log(sum(1)(2)(3)());
+console.log(sum(1)(2)(3)(4)(5)());
